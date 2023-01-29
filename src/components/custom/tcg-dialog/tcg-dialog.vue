cb3d757ef8f086fe38acf042d7e8b37b2a2ae98d
FliPPeDround <734243792@qq.com>
2023-01-29 14:34:26 +0800

    chore: update README

--- src/components/custom/tcg-dialog/tcg-dialog.vue
+++ src/components/custom/tcg-dialog/tcg-dialog.vue
@@ -28,7 +28,7 @@ watch(
     ref="dialog"
     p-0 shadow-2xl bg="white/0"
   >
-    <div @click.stop>
+    <div @click.stop shadow-2xl>
       <slot />
     </div>
   </dialog>
@@ -37,22 +37,5 @@ watch(
 <style scoped>
 dialog::backdrop{
   background-color: rgba(0, 0, 0, 0.5);
-}
-.bounce-enter-active {
-  animation: bounce-in 0.5s;
-}
-.bounce-leave-active {
-  animation: bounce-in 0.5s reverse;
-}
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.25);
-  }
-  100% {
-    transform: scale(1);
-  }
 }
 </style>
