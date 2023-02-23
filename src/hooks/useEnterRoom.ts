141bba0ee0b3fe52fb3e541ccf5429bbf43238b4
FliPPeDround <734243792@qq.com>
2023-02-23 17:46:16 +0800

    feat import peerjs

--- src/hooks/useEnterRoom.ts
+++ src/hooks/useEnterRoom.ts
@@ -1,8 +1,18 @@
+import { Peer } from 'peerjs'
+
 export const useEnterRoom = () => {
   const { data, send } = ws()
+  const peer = new Peer()
+  let peerId = ''
+
+  peer.on('open', (id) => {
+    console.log(`My peer ID is: ${id}`)
+    peerId = id
+  })
 
   const sendCommand = JSON.stringify({
     command: 'create',
+    id: peerId,
   })
 
   send(sendCommand)
