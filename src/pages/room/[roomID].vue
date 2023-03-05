781ac3ee21f2e6c8435a04ea074e36e714d6f755
flippedround <734243792@qq.com>
2023-03-05 16:44:22 +0800

    feat: share button

--- src/pages/room/[roomID].vue
+++ src/pages/room/[roomID].vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 const props = defineProps<{ roomID: string }>()
-// const router = useRouter()
+const router = useRouter()
+const { share } = useShare({
+  title: '快来和我一起玩《七圣召唤》吧！',
+  text: props.roomID,
+  url: window.location.href,
+})
 
 const copyRoomID = () => {
   navigator.clipboard.writeText(props.roomID)
@@ -36,19 +41,32 @@ const copyRoomID = () => {
     </div>
     <div
       w-full h-20vh
-      flex-center
+      flex="center col"
       cursor-pointer
       @click="copyRoomID"
     >
-      <span
+      <div
+        class="group"
         text="white 8"
         font-600 flex
       >
-        房间号:<span>
+        房间号:
+        <span
+          group-active:text-shadow-xl
+          underline="~ offset-5 opacity-10"
+        >
           {{ props.roomID }}
         </span>
         <div i-carbon-copy text="3" />
-      </span>
+      </div>
+      <div mt-5>
+        <button btn mr-2 @click="share()">
+          分享房间
+        </button>
+        <button btn @click="router.push('/')">
+          退出房间
+        </button>
+      </div>
     </div>
   </div>
 </template>
